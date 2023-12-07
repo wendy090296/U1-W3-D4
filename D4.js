842d7fb5dbe932623d2036666213e18d85f9fd18
Wendy <wendydeluca96@hotmail.com>
2023-12-07 17:27:39 +0100

    changes

--- D4.js
+++ D4.js
@@ -1,5 +1,21 @@
-const masterBoard = document.getElementById("master-board");
-for (i = 0; i < 9; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
+// creo array vuoto e faccio un push dei numeri
+
+const arrayOfNumbers = [];
+
+for (let i = 1; i <= 90; i++) {
+  arrayOfNumbers.push(i);
 }
+console.log(arrayOfNumbers);
+
+const Cells = function (numbers) {
+  const masterBoard = document.getElementById("master-board");
+  for (let i = 0; i < numbers.length; i++) {
+    const numberCellDiv = document.createElement("div");
+    numberCellDiv.classList.add("number");
+    const cellValue = document.createElement("h3");
+    cellValue.innerText = i + 1;
+    numberCellDiv.appendChild(cellValue);
+    masterBoard.appendChild(numberCellDiv);
+  }
+};
+createBingoCells(masterboard);
