8230da0d9ba5db62d82f6330fa0b7ff9577cb37d
Wendy <wendydeluca96@hotmail.com>
2023-12-08 12:58:50 +0100

    changes applied

--- D4.js
+++ D4.js
@@ -1,21 +1,46 @@
-// creo array vuoto e faccio un push dei numeri
+// creo array vuoto che conterrà i 90 numeri e verrà utilizzato per generare i numeri random.
 
 const arrayOfNumbers = [];
-
-for (let i = 1; i <= 90; i++) {
-  arrayOfNumbers.push(i);
-}
 console.log(arrayOfNumbers);
 
-const Cells = function (numbers) {
-  const masterBoard = document.getElementById("master-board");
-  for (let i = 0; i < numbers.length; i++) {
-    const numberCellDiv = document.createElement("div");
+const masterBoard = document.getElementById("master-board"); // seleziono il contenitore dei numeri
+// console.log(masterBoard);
+const cells = function (numbers) {
+  // creo la funzione per generare i numeri e le sue celle.
+  for (let i = 0; i < numbers; i++) {
+    const numberCellDiv = document.createElement("div"); // contenitore della cella
     numberCellDiv.classList.add("number");
-    const cellValue = document.createElement("h3");
+    numberCellDiv.classList.add("cell-" + (i + 1)); // questa classe é per selezionare la cella specifica allo generarsi del numero
+
+    const cellValue = document.createElement("span"); //creo il contenuto della cella (il numero)
     cellValue.innerText = i + 1;
-    numberCellDiv.appendChild(cellValue);
-    masterBoard.appendChild(numberCellDiv);
+    numberCellDiv.appendChild(cellValue); // appendo il numero alla sua cella
+    masterBoard.appendChild(numberCellDiv); // appena la cella al contenitore generale
+    arrayOfNumbers.push(i + 1); // pusho tutti i numeri nell'array d partenza
   }
 };
-createBingoCells(masterboard);
+cells(90);
+
+const estraiNumber = document.getElementById("title2"); // seleziono il bottone estrai numero
+estraiNumber.addEventListener("click", function (e) {
+  if (arrayOfNumbers.length === 0) {
+    // se la lunghezza dell'array =0 (array finito)
+    return alert("Tutti i numeri sono stati estratti ");
+  }
+  console.log("lunghezza array", arrayOfNumbers.length);
+  // gli dico come comportarsi al click
+  const randomNumberIndex = Math.floor(
+    // genero indice random dell'array
+    Math.random() * (arrayOfNumbers.length - 1)
+  );
+  const randomNumber = arrayOfNumbers[randomNumberIndex];
+  console.log("index", randomNumberIndex);
+  console.log("numero random", randomNumber);
+  const selected = document.querySelector(".cell-" + randomNumber); // seleziono la cella con il numero estratto
+  selected.classList.add("selected");
+  //   console.log(selected);
+  document.getElementById("extracted-number").innerText = randomNumber; // mostro il numero estratto
+
+  // faccio in modo che non venga estratto lo stesso numero 2 volte
+  arrayOfNumbers.splice(randomNumberIndex, 1);
+});
